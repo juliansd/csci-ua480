b409589b00e0bc9755ad55547701da0c00297cb6
Julian Smith De Niro <jsd410@nyu.edu>
2018-06-28 19:07:25 -0400

    attempt at acceleration and deceleration

---
+++ Assets/Assignments/Assignment_04/A04_jsd410/Scripts/AccelerationDeceleration.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace jsd410 {
+
+    public class AccelerationDeceleration : MonoBehaviour {
+
+        private float currentSpeed = 0f;
+        public float maxSpeed;
+        bool mouseDown;
+    	// Use this for initialization
+    	void Start () {
+    		
+    	}
+    	
+    	// Update is called once per frame
+    	void Update () {
+            if (Input.GetMouseButton(0)) {
+                if (currentSpeed < maxSpeed) {
+        		  transform.position = (new Vector3(0, 1.5f, currentSpeed * Time.deltaTime));
+                  currentSpeed += 0.1f;
+                } else {
+                    transform.position = (new Vector3(0, 1.5f, maxSpeed * Time.deltaTime));
+                }
+            }
+    	}
+    }
+}
