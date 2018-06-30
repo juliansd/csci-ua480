d3dfbab3c912b2bcb2930ecc0d2d7917c8d4966a
Jarred Anthony <Jarredvdv@users.noreply.github.com>
2018-06-30 08:40:55 -0400

    jvd309 - Assignment 3 (#91)
    * a2
    
    * clean up assignments
    
    * cleaned up structure
    
    * added gitignore for unity
    
    * resolve merge conflict
    
    * resolve merge conflict
    
    * resolve merge conflict
    
    * resolve merge conflict
    
    * resolve merge conflict
    
    * resolve merge conflict
    
    * resolve merge conflict
    
    * resolve merge conflict
    
    * resolve merge conflict
    
    * resolve merge conflict
    
    * resolve merge conflict
    
    * resolve merge conflict
    
    * a1 cleanup
    
    * clean up a1
    
    * adjusted namespaces
    
    * resolved merge conflict
    
    * resolved merge conflict
    
    * a2 fixed scene
    
    * assignment 3 fetch from upstream
    
    * added folders for Assignment_04
    
    * Assignment_04 Player Movement Examples
    
    * modified player move-to-target example; fixed gitignore
    
    * Tilting Movement Example
    
    * reconnect scripts to objects in movement examples
    
    * assignment 3
    
    * assignment3
    
    * assignment 3
    
    * merge conflict
    
    * merge conflict
    
    * assignment 3
    
    * assignment 3
    
    * assignment 3
    
    * assignment 3
    
    * resolve merge conflict
    
    * remove .orig
    
    * Delete RotateMe.cs.meta
    
    * resolve merge conflicts
    
    * resolve merge conflicts
    
    * resolve merge conflicts
    
    * remove .orig
    
    * fix scenes
    
    * fix scenes
    
    * fix scenes
    
    * fix scenes
    
    * fix scenes
    
    * resolve merge conflicts
    
    * resolve merge conflicts
    
    * resolve merge conflicts
    
    * fix ProjectVersion.txt

---
+++ Assets/Assignments/Assignment_03/A03_jvd309/Scripts/modeHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace jvd309
+{
+    public class modeHandler : MonoBehaviour
+    {
+        rotateMe RotateMe;
+        PickupMe PickupMe;
+        // Use this for initialization
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+        public void CheckMode(){
+            //When the cube is clicked disable picking up of cube
+            PickupMe = FindObjectOfType<PickupMe>();
+            PickupMe.enabled = false;
+
+            //Start rotate script and initialize new values
+            RotateMe = FindObjectOfType<rotateMe>();
+            RotateMe.enabled = true;
+            RotateMe.Start();
+            RotateMe.RotateOrDrop();
+            RotateMe.Update();
+        }
+    }
+}
