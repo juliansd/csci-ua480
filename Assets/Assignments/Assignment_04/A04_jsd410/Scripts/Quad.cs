66f3f50290b1caece974cdce5f5221b48433c03f
Julian Smith De Niro <jsd410@nyu.edu>
2018-07-03 18:51:31 -0400

    changed folder structure

---
+++ Assets/Assignments/Assignment_04/A04_jsd410/Scripts/Quad.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace jsd410 {
+
+    public class Quad : MonoBehaviour {
+
+    	// Use this for initialization
+    	void Start () {
+    		Mesh mesh = new Mesh();
+            GetComponent<MeshFilter>().mesh = mesh;
+
+            Vector3[] vertices = new Vector3[4];
+
+            vertices[0] = new Vector3(-1, 1, 0);
+            vertices[1] = new Vector3(1, 1, 0);
+            vertices[2] = new Vector3(1, -1, 0);
+            vertices[3] = new Vector3(-1, -1, 0);
+
+            int[] triangles = new int[] {0, 1, 3, 3, 1, 2};
+
+            mesh.vertices = vertices;
+            mesh.triangles = triangles;
+    	}
+    	
+    	// Update is called once per frame
+    	void Update () {
+    		
+    	}
+    }
+}
