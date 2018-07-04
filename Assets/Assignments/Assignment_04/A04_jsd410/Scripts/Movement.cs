22aafa56b1c00521a3ee6fcc22ee82e12eefe556
Julian Smith De Niro <jsd410@nyu.edu>
2018-07-03 22:56:38 -0400

    completed part 1

---
+++ Assets/Assignments/Assignment_04/A04_jsd410/Scripts/Movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace jsd410 {
+
+    public class Movement : MonoBehaviour {
+
+        private float timeButtonIsPressed = 0;
+        public float topSpeed;
+
+        void Update() {
+            if (Input.GetMouseButtonDown(0)) {
+
+                // Does Teleport
+                if (timeButtonIsPressed < 0.1) {
+                    RaycastHit hit;
+                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                    if (Physics.Raycast(ray.origin, ray.direction, out hit, float.PositiveInfinity)) {
+                        GameObject plane = hit.collider.gameObject;
+                        if(plane.tag == "Plane") {
+                            transform.position = hit.point;
+                        }
+                        if (transform.position.y < 0.5) {
+                            transform.position = new Vector3(transform.position.x, 0.5f, transform.position.z);
+                        }
+                    }
+                } else {
+                    RaycastHit hit;
+                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                    if (Physics.Raycast(ray.origin, ray.direction, out hit, float.PositiveInfinity)) {
+                        GameObject plane = hit.collider.gameObject;
+                        if(plane.tag == "Plane") {
+                            transform.position = hit.point;
+                        }
+                        if (transform.position.y < 0.5) {
+                            transform.position = new Vector3(transform.position.x, 0.5f, transform.position.z);
+                        }
+                    }
+                }
+            }
+            timeButtonIsPressed = 0;
+        }
+        
+    }
+}
